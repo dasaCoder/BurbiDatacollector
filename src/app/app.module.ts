ae7cd3d1666617d0cf3f58f8f1f776e1dca584ee
BurbaCodes <dilushadasanayaka@gmail.com>
2020-01-18 13:38:30 +0530

    add angular fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatStepperModule, MatButtonModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WizardComponent } from './wizard/wizard.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WizardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig, 'burba-datacollector'),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,19 +3,20 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyA_BaTF1OO_InXzGoVXD1LVfxDrVvEbH-4",
+    authDomain: "burba-datacollector.firebaseapp.com",
+    databaseURL: "https://burba-datacollector.firebaseio.com",
+    projectId: "burba-datacollector",
+    storageBucket: "burba-datacollector.appspot.com",
+    messagingSenderId: "208947321924",
+    appId: "1:208947321924:web:9b906bab262c2694bfb011",
+    measurementId: "G-VKB8JJ5RV1"
+  }
 };
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyA_BaTF1OO_InXzGoVXD1LVfxDrVvEbH-4",
-  authDomain: "burba-datacollector.firebaseapp.com",
-  databaseURL: "https://burba-datacollector.firebaseio.com",
-  projectId: "burba-datacollector",
-  storageBucket: "burba-datacollector.appspot.com",
-  messagingSenderId: "208947321924",
-  appId: "1:208947321924:web:9b906bab262c2694bfb011",
-  measurementId: "G-VKB8JJ5RV1"
-};
+
 
 /*
  * For easier debugging in development mode, you can import the following file
