00a15e4b58ef69e8a8113e8b7bbc7ab1686b29a8
BurbaCodes <dilushadasanayaka@gmail.com>
2020-01-19 18:05:28 +0530

    first page done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyA_BaTF1OO_InXzGoVXD1LVfxDrVvEbH-4",
+    authDomain: "burba-datacollector.firebaseapp.com",
+    databaseURL: "https://burba-datacollector.firebaseio.com",
+    projectId: "burba-datacollector",
+    storageBucket: "burba-datacollector.appspot.com",
+    messagingSenderId: "208947321924",
+    appId: "1:208947321924:web:9b906bab262c2694bfb011",
+    measurementId: "G-VKB8JJ5RV1"
+  }
 };
